77a5223a5cfdb0bdfc59f0b3e8ed01ab724d72e4
Lusmaysh <arjunoimut123@gmail.com>
2024-12-07 14:41:36 +0700

    add file pertemuan7

---
+++ pertemuan7/src/main/Kelas.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+/**
+ *
+ * @author PC
+ */
+public class Kelas {
+    
+    private String nama;
+    private String jenisKelamin;
+    private String kelasPelatihan;
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getJenisKelamin() {
+        return jenisKelamin;
+    }
+
+    public void setJenisKelamin(String jenisKelamin) {
+        this.jenisKelamin = jenisKelamin;
+    }
+
+    public String getKelasPelatihan() {
+        return kelasPelatihan;
+    }
+
+    public void setKelasPelatihan(String kelasPelatihan) {
+        this.kelasPelatihan = kelasPelatihan;
+    }
+    
+    
+            
+}
